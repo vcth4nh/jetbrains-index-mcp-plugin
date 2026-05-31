e177d3d1ad9b952701a37cfd669ef4d9faf07108
vcth4nh <tvu25337@gmail.com>
2026-05-31 21:02:24 +0700

    test(c1): consolidate errorType tests into McpExceptionsUnitTest

--- src/test/kotlin/com/github/hechtcarmel/jetbrainsindexmcpplugin/exceptions/McpExceptionsUnitTest.kt
+++ src/test/kotlin/com/github/hechtcarmel/jetbrainsindexmcpplugin/exceptions/McpExceptionsUnitTest.kt
@@ -178,4 +178,24 @@ class McpExceptionsUnitTest : TestCase() {
                 code in -32099..-32001)
         }
     }
+
+    // errorType tests
+
+    fun testErrorTypeMappings() {
+        assertEquals("index_not_ready", IndexNotReadyException("x").errorType)
+        assertEquals("file_not_found", FileNotFoundException("/p").errorType)
+        assertEquals("symbol_not_found", SymbolNotFoundException("x").errorType)
+        assertEquals("refactoring_conflict", RefactoringConflictException("x").errorType)
+        assertEquals("invalid_params", InvalidParamsException("x").errorType)
+        assertEquals("internal_error", InternalErrorException("x").errorType)
+        assertEquals("parse_error", ParseErrorException("x").errorType)
+        assertEquals("invalid_request", InvalidRequestException("x").errorType)
+        assertEquals("method_not_found", MethodNotFoundException("m").errorType)
+    }
+
+    fun testErrorTypeCoexistsWithErrorCode() {
+        val e: McpException = IndexNotReadyException("x")
+        assertEquals("index_not_ready", e.errorType)
+        assertEquals(JsonRpcErrorCodes.INDEX_NOT_READY, e.errorCode)
+    }
 }
